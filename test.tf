128db4531f5cc37b3c2bac5358f303333b691948
devopspracticeb71 <doppalapudisrikanth@gmail.com>
2023-02-23 10:31:40 +0000

    error changed

--- test.tf
+++ test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
-    vpc_security_group_ids ="[sg-0ece77255d5afe5df]"
+    vpc_security_group_ids = "[sg-0ece77255d5afe5df]"
     tags = {
         name = "test"
     }
