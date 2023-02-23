92b7372683faab1449dffd29e3af945fb23113ec
devopspracticeb71 <doppalapudisrikanth@gmail.com>
2023-02-23 10:58:56 +0000

    error changed

--- test.tf
+++ test.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "ec2" {
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-0ece77255d5afe5df"]
     tags = {
-        name = "test"
+        Name = "test"
     }
 }
