275adc53cfada89bc4f12a18627bc9804b44bdea
devopspracticeb71 <doppalapudisrikanth@gmail.com>
2023-02-23 12:58:03 +0000

    output test change3

--- output/output.tf
+++ output/output.tf
@@ -2,6 +2,6 @@ output "sample" {
 value = "hello"
 }
 
-output "instance_ip_adress" {
-    value = "aws_instance.server.private_ip"
+output "instance_ip_addr" {
+    value = aws_instance.server.private_ip
 }
